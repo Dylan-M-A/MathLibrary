20a34bee0755c557781e8eefdbd54860f158e6c0
Dylan-M-A <darmentorbusiness@gmail.com>
2024-10-09 10:47:48 -0500

    tested the projects

---
+++ Sandbox/Program.cs
@@ -0,0 +1,13 @@
+﻿using MathLibrary;
+
+namespace Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1();
+            a.Test();
+        }
+    }
+}
