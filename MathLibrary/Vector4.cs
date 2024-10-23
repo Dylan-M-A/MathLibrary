78c262a076eab7d16ccad7cac474607d944f97f4
Dylan-M-A <darmentorbusiness@gmail.com>
2024-10-23 09:51:44 -0500

    made t in vector to w

--- MathLibrary/Vector4.cs
+++ MathLibrary/Vector4.cs
@@ -8,14 +8,14 @@ namespace MathLibrary
 {
     public struct Vector4
     {
-        public float x, y, z, t;
+        public float x, y, z, w;
 
         public float Magnitude
         {
             get
             {
                 //(x*x+y*y+z*z+t*t)
-                return (float)Math.Abs(Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2) + Math.Pow(t, 2)));
+                return (float)Math.Abs(Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2) + Math.Pow(w, 2)));
             }
         }
         public Vector4 Normalized
@@ -27,22 +27,22 @@ namespace MathLibrary
         }
         public override string ToString()
         {
-            return "(" + x + ", " + y + ", " + z + ", " + t + ")";
+            return "(" + x + ", " + y + ", " + z + ", " + w + ")";
         }
         public float DotProduct(Vector4 other)
         {
-            return (x * other.x) + (y * other.y) + (z * other.z) + (t * other.t);
+            return (x * other.x) + (y * other.y) + (z * other.z) + (w * other.w);
         }
         public Vector4 CrossProduct(Vector4 left, Vector4 right)
         {
-            return new Vector4((left.y * right.z) - (left.z * right.y), (left.z * right.t) - (left.t * right.z), (left.t * right.x) - (left.x * right.t), (left.x * right.y) - (left.y * right.x));
+            return new Vector4((left.y * right.z) - (left.z * right.y), (left.z * right.w) - (left.w * right.z), (left.w * right.x) - (left.x * right.w), (left.x * right.y) - (left.y * right.x));
         }
-        public Vector4(float x = 0, float y = 0, float z = 0, float t = 0)
+        public Vector4(float x = 0, float y = 0, float z = 0, float w = 0)
         {
             this.x = x;
             this.y = y;
             this.z = z;
-            this.t = t;
+            this.w = w;
         }
         public Vector4 Normalize()
         {
@@ -51,7 +51,7 @@ namespace MathLibrary
         }
         public static bool operator ==(Vector4 lhs, Vector4 rhs)
         {
-            return (lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z && lhs.t == rhs.t);
+            return (lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z && lhs.w == rhs.w);
         }
         public static bool operator !=(Vector4 lhs, Vector4 rhs)
         {
@@ -60,33 +60,33 @@ namespace MathLibrary
         //operater overload for addition
         public static Vector4 operator +(Vector4 left, Vector4 right)
         {
-            return new Vector4(left.x + right.x, left.y + right.y, left.z + right.z, left.t + right.t);
+            return new Vector4(left.x + right.x, left.y + right.y, left.z + right.z, left.w + right.w);
         }
         public static Vector4 operator -(Vector4 left, Vector4 right)
         {
-            return new Vector4(left.x - right.x, left.y - right.y, left.z - right.z, left.t - right.t);
+            return new Vector4(left.x - right.x, left.y - right.y, left.z - right.z, left.w - right.w);
         }
         //operator overload for multiplication by a scalar
         public static Vector4 operator *(Vector4 left, float scalar)
         {
-            return new Vector4(left.x * scalar, left.y * scalar, left.z * scalar, left.t * scalar);
+            return new Vector4(left.x * scalar, left.y * scalar, left.z * scalar, left.w * scalar);
         }
 
         public static Vector4 operator /(Vector4 left, float scalar)
         {
-            return new Vector4(left.x / scalar, left.y / scalar, left.z / scalar, left.t / scalar);
+            return new Vector4(left.x / scalar, left.y / scalar, left.z / scalar, left.w / scalar);
         }
 
         //implicit conversion from System.Numerics.Vector4 to Vector4
         public static implicit operator Vector4(System.Numerics.Vector4 vector)
         {
-            return new Vector4(vector.X, vector.Y, vector.Z);
+            return new Vector4(vector.X, vector.Y, vector.Z, vector.W);
         }
 
         //implicit conversion from Vector4 to System.Numerics.Vector4
         public static implicit operator System.Numerics.Vector4(Vector4 vector)
         {
-            return new System.Numerics.Vector4(vector.x, vector.y, vector.z, vector.t);
+            return new System.Numerics.Vector4(vector.x, vector.y, vector.z, vector.w);
         }
     }
 }
